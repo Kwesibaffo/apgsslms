4f596a3a22a6e3c2201ca21e20e1e46a8d66ee8f
Kwesibaffo <109483209+Kwesibaffo@users.noreply.github.com>
2022-07-24 11:18:34 +0000

    admin header index and script

--- header.php
+++ header.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html class="no-js">
     <head>
-        <title>AGPSS LMS</title>
-		<meta name="description" content="Learning Management System">
-		<meta name="keywords" content="Archbishop Girls' Secondary School is located in the western part of Ghana. It is a girl's school and have been able to produce so students who are prominent people in the Ghanaian Society to day. This lms is designed to help student learn at their own pace">
-		<meta name="author" content="Archbishop Girls' Secondary School">
-		<meta charset="UTF-8">
+        <title>Learning Management System</title>
         <!-- Bootstrap -->
-				<link href="admin/images/Logo.png" rel="icon" type="image">
-				<link href="admin/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
-				<link href="admin/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen"/>
-				<link href="admin/bootstrap/css/font-awesome.css" rel="stylesheet" media="screen"/>
-				<link href="admin/bootstrap/css/my_style.css" rel="stylesheet" media="screen"/>
-				<link href="admin/vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen"/>
-				<link href="admin/assets/styles.css" rel="stylesheet" media="screen"/>
-				<!-- calendar css -->
-				<link href="admin/vendors/fullcalendar/fullcalendar.css" rel="stylesheet" media="screen">
-				<!-- index css -->
-				<link href="admin/bootstrap/css/index.css" rel="stylesheet" media="screen"/>
-				<!-- data table css -->
-				<link href="admin/assets/DT_bootstrap.css" rel="stylesheet" media="screen"/>
-				<!-- notification  -->
-				<link href="admin/vendors/jGrowl/jquery.jgrowl.css" rel="stylesheet" media="screen"/>
-				<!-- wysiwug  -->
-				<link rel="stylesheet" type="text/css" href="admin/vendors/bootstrap-wysihtml5/src/bootstrap-wysihtml5.css"/>
-		<script src="admin/vendors/jquery-1.9.1.min.js"></script>
-        <script src="admin/vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+			<link href="images/logo.png" rel="icon" type="image">
+			<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+			<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+			<link href="bootstrap/css/font-awesome.css" rel="stylesheet" media="screen">
+			<link href="bootstrap/css/my_style.css" rel="stylesheet" media="screen">
+			<link href="vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen">
+			<link href="assets/styles.css" rel="stylesheet" media="screen">
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+		<!-- calendar css -->
+		<link href="vendors/fullcalendar/fullcalendar.css" rel="stylesheet" media="screen">
+		<script src="vendors/jquery-1.9.1.min.js"></script>
+        <script src="vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<!-- data table -->
+		<link href="assets/DT_bootstrap.css" rel="stylesheet" media="screen">
+		<!-- notification  -->
+		<link href="vendors/jGrowl/jquery.jgrowl.css" rel="stylesheet" media="screen">
+		<!-- wysiwug  -->
+		<link rel="stylesheet" type="text/css" href="vendors/bootstrap-wysihtml5/src/bootstrap-wysihtml5.css"></link>
+	
+		<script src="vendors/jGrowl/jquery.jgrowl.js"></script>
     </head>
-<?php ?>
+<?php //include('dbcon.php'); ?>
--- index.php
+++ index.php
@@ -1,31 +1,41 @@
 <?php include('header.php'); ?>
 <style>
-	body#login::before {
-    content: "";
-    background: #00000036;
-    position: absolute;
-    top: 0;
-    /* z-index: 1; */
-    left: 0;
+  .input100 {
+    font-family: Poppins-Regular;
+    font-size: 15px;
+    color: #555555;
+    line-height: 1.2;
+
+    display: block;
     width: 100%;
-    height: 100%;
-}
-	
+    height: 45px;
+    background: transparent;
+    padding: 0 5px;
+  }
 </style>
-<body id="login">
-    <div class="container" style="position: relative">
-		<div class="row-fluid">
-			<div class="span6"><div class="title_index"><?php include('title_index.php'); ?></div></div>
-			<div class="span6"><div class="pull-right"><?php include('login_form.php'); ?></div></div>
+  <body id="login" style="background-image: url('images/bg.jpg')">
+    <div class="container">
+
+      <form id="login_form" class="form-signin" method="post" style="max-width: 400px !important; height: 480px;background:rgba(255,255,255,0.5)!important">
+       <center> <h3 class="form-signin-heading"><i class="icon-lock"></i> ADMIN LOGIN</h3></center>
+        <center>
+        <img src="images/logo.png" width="110px" height="110px">
+      </center> <br><br>
+        <div class="row" style="width:80%!important;margin:0 auto;">
+        <div class="col-md-12">
+        <input type="text" class="input-block-level input100" id="usernmae" name="username" placeholder="Username" required>
+        <input type="password" class="input-block-level" id="password" name="password" placeholder="Password" required>
+        <button name="login" class="btn btn-info" type="submit"><i class="icon-signin icon-"></i> Sign in</button>
 		</div>
-		<div class="row-fluid">
-			<div class="span12"><div class="index-footer">
-			
-					
-				</div></div>
-		</div>
-			<?php include('footer.php'); ?>
-    </div>
+
+	</div>
+
+		      </form>
+		
+		
+	<center><p style="color: white;">ALL RIGHT RESERVED &copy; <?php echo date("Y");?></p></center>
+
+    </div> <!-- /container -->
 <?php include('script.php'); ?>
-</body>
+  </body>
 </html>
